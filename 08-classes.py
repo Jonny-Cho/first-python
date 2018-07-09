b1d59e2685226d24a071e5b9e5f89c80ca4b1041
Jonny <andante2183@gmail.com>
2018-07-09 19:58:05 +0900

    classes-3

--- 08-classes.py
+++ 08-classes.py
@@ -46,6 +46,18 @@ article1 = Article("개발", "개발은 쉬워요")
 article2 = Article("코칭", "코칭은 쉬워요")
 article3 = Article("창업", "창업은 쉬워요")
 
-print(article1.view_count)
-article1.read()
-print(article1.view_count)
+# print(article1.view_count)
+# article1.read()
+# print(article1.view_count)
+
+
+#### Article class inheritance 상속
+class BrunchArticle(Article):
+    source = "브런치"
+
+brunch_article = BrunchArticle("개발", "개발은 쉬워요")
+print(brunch_article.title)
+print(brunch_article.source)
+print(brunch_article.view_count)
+brunch_article.read()
+print(brunch_article.view_count)
