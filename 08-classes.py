431ddb71149c7e18e2853fb87673af03be9fa41c
Jonny <andante2183@gmail.com>
2018-07-09 19:37:28 +0900

    classes-default

---
+++ 08-classes.py
@@ -0,0 +1,17 @@
+# 클래스 class
+
+#### Article variables
+title1 = "개발"
+author1 = "마르코"
+content1 = "개발은 쉬워요"
+view_count1 = 0
+
+title2 = "코칭"
+author2 = "마르코"
+content2 = "코칭은 쉬워요"
+view_count2 = 0
+
+title3 = "창업"
+author3 = "마르코"
+content3 = "창업은 쉬워요"
+view_count3 = 0
