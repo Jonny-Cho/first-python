395a7b0daa1b743b71e12efc84835fddfcf89aab
Jonny <andante2183@gmail.com>
2018-07-11 20:19:09 +0900

    modified assignment-03

--- assignment-03-rockscissorpaper.py
+++ assignment-03-rockscissorpaper.py
@@ -12,28 +12,43 @@
 
 import random
 
-user_choice = input("가위, 바위, 보 중 하나를 입력해 주세요")
-computer_choice = random.choice(["가위", "바위", "보"])
+ROCK = "바위"
+SCISSORS = "가위"
+PAPER = "보"
+RSP_LIST = (
+    ROCK,
+    SCISSORS,
+    PAPER
+)
+
+win_count = 0
+lose_count = 0
 
 #Result
-
-if user_choice == computer_choice:
-    print ("비겼습니다!")
-elif user_choice == "가위":
-    if computer_choice == "보":
-        print("이겼습니다")
-    else:
-        print("졌습니다")
-elif user_choice == "바위":
-    if computer_choice == "가위":
-        print("이겼습니다")
-    else:
-        print("졌습니다")
-elif user_choice == "보":
-    if computer_choice == "바위":
-        print("이겼습니다")
-    else:
-        print("졌습니다")
-
-
+while win_count < 3 and lose_count < 3:
+    user_choice = input("{}, {}, {}: ".format(
+        SCISSORS, ROCK, PAPER
+        ))
+    computer_choice = random.choice(RSP_LIST)
     
+    if user_choice == computer_choice:
+        print ("비겼습니다!")
+    elif user_choice not in RSP_LIST:
+        print("가위, 바위, 보 중에서 하나를 입력해 주세요.")
+    elif ((user_choice == ROCK and computer_choice == SCISSORS)
+        or (user_choice == SCISSORS and computer_choice == PAPER)
+        or (user_choice == PAPER and computer_choice == ROCK)
+        ):
+        win_count = win_count + 1
+        print("이겼습니다")
+    else:
+        lose_count = lose_count + 1
+        print("졌습니다")
+
+if win_count == 3:
+    print("사용자가 최종 승리하였습니다.")
+elif lose_count == 3:
+    print("컴퓨터가 최종 승리하였습니다.")
+
+
+        
